e296400a13cf4aeb5fb9ce636b6009ca0d074e0e
outofband1 <40806227+outofband1@users.noreply.github.com>
2018-07-03 23:07:22 +0200

    almost done

--- utitl/utitl.cpp
+++ utitl/utitl.cpp
@@ -14,57 +14,31 @@ template <typename TYPE, int ROWS, int COLUMNS>
 class SimplexTableau
 {
 public:
-	void setRow(const int& row, const TYPE values[COLUMNS])
-	{
-		memcpy(&entries[row * COLUMNS], values, COLUMNS * sizeof(TYPE));
-	}
-
 	void setTableau(const TYPE values[COLUMNS * ROWS])
 	{
 		memcpy(entries, values, ROWS * COLUMNS * sizeof(TYPE));
 	}
 
 	inline
-	const TYPE& getEntry(const int& row, const int& col)
+		const TYPE& getEntry(const size_t& row, const size_t& col)
 	{
 			return entries[row * COLUMNS + col];
 		}
 
 	inline
-	void setEntry(const int& row, const int& col, const TYPE& val)
+		void setEntry(const size_t& row, const size_t& col, const TYPE& val)
 	{
 			entries[row * COLUMNS + col] = val;
-		}
-
-	inline
-	void pivot(const int& row, const int& col)
-	{
-			const TYPE& multValue = 1 / getEntry(row, col);
-			multiplyRow(row, multValue);
-
-			for (int i = 0; i < ROWS; i++)
-			{
-				if (i != row && getEntry(i, col) != 0)
-				{
-					TYPE pivotMult = getEntry(i, col);
-					for (int j = 0; j < COLUMNS; j++)
-					{
-						TYPE newValue = getEntry(i, j) - pivotMult * getEntry(row, j);
-						setEntry(i, j, newValue);
-					}
-				}
-
-			}
-		}
+	}
 
 	void print()
 	{
 		std::cout.precision(2);
 		std::cout << std::fixed << std::showpos;
 		std::cout << std::endl;
-		for (int i = 0; i < ROWS; i++) // row
+		for (size_t i = 0; i < ROWS; i++) // row
 		{
-			for (int j = 0; j < COLUMNS; j++) // column
+			for (size_t j = 0; j < COLUMNS; j++) // column
 			{
 				std::cout << getEntry(i, j);
 				if (j < COLUMNS - 1)
@@ -77,13 +51,88 @@ public:
 		}
 		std::cout << std::endl;
 	}
+
+	void Solve()
+	{
+		bool done = false;
+		while (!done)
+		{
+			print();
+			// find entering variable / pivot column
+			size_t enteringColumnIndex = -1;
+			for (size_t i = 0; i < COLUMNS; i++)
+			{
+				if (getEntry(0, i) < 0)
+				{
+					enteringColumnIndex = i;
+					break;
+				}
+			}
+
+			if (enteringColumnIndex == -1)
+			{
+				done = true;
+				continue;
+			}
+
+			// find pivot row
+			int pivotRow = -1;
+			TYPE smallestRatio = std::numeric_limits<TYPE>::max();
+			for (size_t i = 1; i < ROWS; i++)
+			{
+				TYPE evc = getEntry(i, enteringColumnIndex); // entering variable coefficient
+				if (evc <= 0)
+					continue;
+				TYPE rhs = getEntry(i, COLUMNS-1); // right hand side
+				
+				TYPE newRatio = rhs / evc;
+				
+				if (newRatio < smallestRatio)
+				{
+					smallestRatio = newRatio;
+					pivotRow = i;
+				}
+			}
+
+			if (pivotRow == -1)
+			{
+				std::cout << "Problem unbounded" << std::endl;
+				return; // all coefficients negative -> problem unbounded
+			}
+				
+
+			pivot(pivotRow, enteringColumnIndex);
+		}
+	}
+
 private:
 	inline
-		void multiplyRow(const int& row, const TYPE& multiplier)
+		void multiplyRow(const size_t& row, const TYPE& multiplier)
 	{
-			for (int i = row * COLUMNS; i < row * COLUMNS + COLUMNS; i++)
+		for (size_t i = row * COLUMNS; i < row * COLUMNS + COLUMNS; i++)
+		{
+			entries[i] *= multiplier;
+		}
+	}
+
+	inline
+		void pivot(const size_t& row, const size_t& col)
+	{
+			const TYPE& multValue = 1 / getEntry(row, col);
+			multiplyRow(row, multValue);
+
+			for (size_t i = 0; i < ROWS; i++)
 			{
-				entries[i] *= multiplier;
+				if (i != row && getEntry(i, col) != 0)
+				{
+					TYPE pivotMult = getEntry(i, col);
+					for (size_t j = 0; j < COLUMNS; j++)
+					{
+						TYPE newValue = getEntry(i, j) - pivotMult * getEntry(row, j);
+						setEntry(i, j, newValue);
+					}
+				}
+
 			}
 		}
 
@@ -92,31 +141,20 @@ private:
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	SimplexTableau<float, 4, 6> m;
+	SimplexTableau<float, 3, 5> m;
 
 	float data[] =
 	{
-		-2, -1, +0, +0, +0, +0,
-		+3, +1, +1, +0, +0, +6,
-		+1, -1, +0, +1, +0, +2,
-		+0, +1, +0, +0, +1, +3
+		-2, -1, +0, +0, +0,
+		-1, +1, +1, +0, +1,
+		+1, -2, +0, +1, +2
 	};
 
 	m.setTableau(data);
 
-	m.print();
+	m.Solve();
 
-	m.pivot(2, 0);
-
-	m.print();
-
-	m.pivot(1, 1);
-
-	m.print();
-
-	m.pivot(3, 3);
-
-	m.print();
+	
 
 
 	std::cout << std::endl;
