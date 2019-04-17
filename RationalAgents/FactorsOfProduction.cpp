7050f0cbd0d184d5962563e665815acfaae23c24
outofband1 <40806227+outofband1@users.noreply.github.com>
2019-04-17 12:23:12 +0200

    Create FactorsOfProduction.cpp
    wip

---
+++ RationalAgents/FactorsOfProduction.cpp
@@ -0,0 +1,12 @@
+#include "FactorsOfProduction.h"
+
+FactorsOfProduction::FactorsOfProduction(const double labour) :
+    labour_(labour)
+{
+
+}
+
+double FactorsOfProduction::getLabour() const
+{
+    return labour_;
+}
